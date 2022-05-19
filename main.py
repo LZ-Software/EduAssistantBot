30307c330606e68985324a73127b4c933f944354
Ilya Lugovoy <ilya.l2013@yandex.ru>
2022-05-19 16:48:33 +0300

    Simple bot with protected auth

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-def main():
-    pass
+import json
+import telebot
 
 
-if __name__ == '__main__':
-    main()
+def get_token(filename):
+    with open(filename, 'r', encoding="utf-8") as file:
+        json_file = json.load(file)
+        return json_file['token']
+
+
+bot = telebot.TeleBot(token=get_token('auth.json'))
+
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Здравствуйте")
+
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+
+bot.infinity_polling()
